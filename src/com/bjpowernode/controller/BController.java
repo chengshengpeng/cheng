c85234004873389ee5c6d6f00adcec584c135898
zhangliang <2655904102@qq.com>
2020-12-16 15:59:20 +0800

    添加BController

---
+++ src/com/bjpowernode/controller/BController.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.controller;
+
+/**
+ * @ProjectName: wang
+ * @Package: com.bjpowernode.controller
+ * @Description: java类作用描述
+ * @Author: admin
+ * @CreateDate: 2020/12/16 15:58
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class BController {
+}
