d87ab49f2da188cd63be2dd59972d01458e75286
zhangliang <2655904102@qq.com>
2020-12-16 15:54:58 +0800

    今天提交了一个没有用的类

---
+++ src/com/bjpowernode/controller/AController.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.controller;
+
+/**
+ * @ProjectName: cheng
+ * @Package: com.bjpowernode.controller
+ * @Description: java类作用描述
+ * @Author: admin
+ * @CreateDate: 2020/12/16 15:52
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class AController {
+}
